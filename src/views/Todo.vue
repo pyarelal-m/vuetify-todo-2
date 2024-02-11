d48e706f7841d3d565ef81533a59facec091e0fb
Pyarelal Mallah <pyarelal@z1media.com>
2024-02-11 18:23:30 +0530

    todo list

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -45,6 +45,78 @@
         <v-divider></v-divider>
       </div>
     </v-list>
+    <v-virtual-scroll
+      :items="tasks"
+      :item-height="20"
+      height="100"
+    >
+    <template v-slot:default="{ task }">
+      <v-list-item
+          @click="doneTask(task.id)"
+          :class="{'red': task.done}"
+        >
+          <!-- <template v-slot:default> -->
+            <v-list-item-action>
+              <v-checkbox :input-value="task.done" color="primary"></v-checkbox>
+            </v-list-item-action>
+
+            <v-list-item-content>
+              <v-list-item-title
+                :class="{'text-decoration-line-through': task.done}"
+              >{{task.title}}</v-list-item-title>
+            </v-list-item-content>
+
+            <v-list-item-action>
+              <v-btn
+                icon
+                @click.stop="deleteTask(task.id)"
+              >
+                <v-icon color="grey lighten-1">mdi-delete</v-icon>
+              </v-btn>
+            </v-list-item-action>
+          <!-- </template> -->
+        </v-list-item>
+    </template>
+    </v-virtual-scroll>
+    <v-virtual-scroll
+      :items="items"
+      :item-height="50"
+      height="300"
+    >
+      <template v-slot:default="{ item }">
+        <v-list-item>
+          <v-list-item-avatar>
+            <v-avatar
+              :color="item.color"
+              size="56"
+              class="white--text"
+            >
+              {{ item.initials }}
+            </v-avatar>
+          </v-list-item-avatar>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.fullName }}</v-list-item-title>
+          </v-list-item-content>
+
+          <v-list-item-action>
+            <v-btn
+              depressed
+              small
+            >
+              View User
+
+              <v-icon
+                color="orange darken-4"
+                right
+              >
+                mdi-open-in-new
+              </v-icon>
+            </v-btn>
+          </v-list-item-action>
+        </v-list-item>
+      </template>
+    </v-virtual-scroll>
   </div>
 </template>
 
@@ -59,10 +131,36 @@ export default {
 
   data() {
     return {
+      benched: 0,
       newTaskTitle: '',
-      tasks: []
+      // tasks: [],
+      colors: ['#2196F3', '#90CAF9', '#64B5F6', '#42A5F5', '#1E88E5', '#1976D2', '#1565C0', '#0D47A1', '#82B1FF', '#448AFF', '#2979FF', '#2962FF'],
+      names: ['Oliver', 'Jake', 'Noah', 'James', 'Jack', 'Connor', 'Liam', 'John', 'Harry', 'Callum', 'Mason', 'Robert', 'Jacob', 'Jacob', 'Jacob', 'Michael', 'Charlie', 'Kyle', 'William', 'William', 'Thomas', 'Joe', 'Ethan', 'David', 'George', 'Reece', 'Michael', 'Richard', 'Oscar', 'Rhys', 'Alexander', 'Joseph', 'James', 'Charlie', 'James', 'Charles', 'William', 'Damian', 'Daniel', 'Thomas', 'Amelia', 'Margaret', 'Emma', 'Mary', 'Olivia', 'Samantha', 'Olivia', 'Patricia', 'Isla', 'Bethany'],
+      surnames: ['Smith', 'Anderson', 'Clark', 'Wright', 'Mitchell', 'Johnson', 'Thomas', 'Rodriguez', 'Lopez', 'Perez', 'Williams', 'Jackson', 'Lewis', 'Hill', 'Roberts', 'Jones', 'White', 'Lee', 'Scott', 'Turner', 'Brown', 'Harris', 'Walker', 'Green', 'Phillips', 'Davis', 'Martin', 'Hall', 'Adams', 'Campbell', 'Miller', 'Thompson', 'Allen', 'Baker', 'Parker', 'Wilson', 'Garcia', 'Young', 'Gonzalez', 'Evans', 'Moore', 'Martinez', 'Hernandez', 'Nelson', 'Edwards', 'Taylor', 'Robinson', 'King', 'Carter', 'Collins'],
     }
   },
+  computed: {
+      items () {
+        const namesLength = this.names.length
+        const surnamesLength = this.surnames.length
+        const colorsLength = this.colors.length
+
+        return Array.from({ length: 100 }, (k, v) => {
+          const name = this.names[this.genRandomIndex(namesLength)]
+          const surname = this.surnames[this.genRandomIndex(surnamesLength)]
+
+          return {
+            color: this.colors[this.genRandomIndex(colorsLength)],
+            fullName: `${name} ${surname}`,
+            initials: `${name[0]} ${surname[0]}`,
+          }
+        })
+      },
+      tasks () {
+        return []
+      }
+    },
+
   methods: {
     addNewTask() {
       this.tasks.push({
@@ -79,7 +177,10 @@ export default {
     },
     deleteTask(id) {
       this.tasks = this.tasks.filter(task => task.id !== id)
-    }
+    },
+    genRandomIndex (length) {
+        return Math.ceil(Math.random() * (length - 1))
+      },
   }
 };
 </script>
